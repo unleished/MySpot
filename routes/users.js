2f2c62a220022f06365f703283f745da2334f4ee
Leisha Magsisi <leisha.gallagher@gmail.com>
2018-08-23 22:00:58 +0000

    updating user info to pull from db and populate on the screen

--- routes/users.js
+++ routes/users.js
@@ -3,38 +3,56 @@
 const express = require('express');
 const router  = express.Router();
 
-
-
-function resultText(rows) {
-  let list = [];
-  rows.forEach(row => {
-    // console.log('result text: ', row.first_name, row.last_name);
-    list.push(row);
-  })
-  return list;
-}
-
-
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      // .then((results) => {
-      //   res.json(results);
-      //   console.log('users.js results: ',results);
-        .asCallback(function(err, rows) {
-          if (err) return console.error(err);
-          // resultText(rows);
-          var userDataObj = {
-            userData: rows
-          }
-          res.render('users_unique', userDataObj);
-          });
-  });
+  router.get("/:id", (req, res) => {
+    Promise.all([
+      knex
+        .select("*")
+        .from("users")
+        .where('id', req.params.id)
+        .then(function(rows) {
+          return rows;
 
+          // var userDataObj = {
+          //   userData: rows
+          // }
+          // res.render('users_unique', userDataObj);
+          // // res.json(rows);
+        })
+        .catch(function (err) {
+          return console.error(err);
+        }),
+    // });
 
+      knex('users')
+        .select('maps.id', 'maps.map_name')
+        .join('user_map', 'users.id', '=', 'user_map.user_id')
+        .join('maps', 'maps.id', '=', 'user_map.map_id')
+        .where('users.id', req.params.id)
+        .then(function(userMapRows) {
+          return userMapRows;
+        }),
+
+      knex('users')
+        .select('maps.id', 'maps.map_name')
+        .join('favorite', 'users.id', '=', 'favorite.user_id')
+        .join('maps', 'maps.id', '=', 'favorite.map_id')
+        .where('users.id', req.params.id)
+        .then(function(userFavMaps) {
+          return userFavMaps;
+        })
+      ])
+      .then((result) => {
+        var userDataObj = {
+          userData: result[0],
+          userMapRows: result[1],
+          userFavs: result[2].length === 0 ? false : result[2]
+        }
+        res.render('users_unique', userDataObj);
+        // res.json(mapDataObj);
+      })
+    })
 
   return router;
 }
