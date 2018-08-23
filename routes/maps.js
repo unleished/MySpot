522799a91115343c03cba45d384bb9fa3c1be0cc
Leisha Magsisi <leisha.gallagher@gmail.com>
2018-08-23 20:05:47 +0000

    adding in the marker to maps and looping through map point data

--- routes/maps.js
+++ routes/maps.js
@@ -21,24 +21,51 @@ module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
 
+    Promise.all([
     knex
       .select("*")
       .from("maps")
       .where('id', req.params.id)
       // .then((resultText(results) => {
       //   res.json(results);
-      .asCallback(function(err, rows) {
-        if (err) return console.error(err);
-        var mapDataObj = {
-          mapInfo: rows,
-        }
+      .then(function(rows) {
 
-        res.render('maps_unique', mapDataObj);
-    });
-  });
+      return rows;
+        // res.json(rows);
+        // res.render('maps_unique', mapDataObj);
+      })
+
+      .catch(function (err) {
+        return console.error(err);
+      }),
+
+    knex('maps')
+      .select('points.point_id', 'points.point_name', 'points.point_description', 'points.point_long', 'points.point_lat')
+      .join('map_points', 'maps.id', '=', 'map_points.map_id')
+      .join('points', 'points.point_id', '=', 'map_points.point_id')
+      .where('maps.id', req.params.id)
+      .then(function(pointRows) {
+
+      return pointRows;
+      })
+
+    ])
+    .then((result) => {
+      var mapDataObj = {
+        mapInfo: result[0],
+        pointInfo: result[1]
+      }
+      res.render('maps_unique', mapDataObj);
+      // res.json(mapDataObj);
+    })
+
+})
+
+  // });
 
 
 
 
   return router;
+
 }
