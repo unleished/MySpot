5f0c87200d29becdb08fe5baffe898d372bca554
Leisha Magsisi <leisha.gallagher@gmail.com>
2018-08-25 19:57:02 +0000

    updating and pulling point info from db to update info

--- routes/points.js
+++ routes/points.js
@@ -41,20 +41,12 @@ module.exports = (knex) => {
 
   router.post("/edit", (req, res) => {
     console.log('req body: ', req.body);
-      // let update_point = {
-      //   point_id: req.body.point_id,
-      //   point_name: req.body.point_name,
-      //   point_description: req.body.point_description,
-      //   point_long: req.body.lng,
-      //   point_lat: req.body.lat,
-      // }
 
         Promise.all([
         knex('points')
         // .update(update_point)
         .update('point_name', req.body.point_name)
         .update('point_description', req.body.point_description)
-        // .update('point_image', 'http://i.stack.imgur.com/g672i.png')
         .update('point_lat', req.body.point_lat)
         .update('point_long', req.body.point_long)
         .where('point_id', req.body.point_id)
@@ -63,6 +55,33 @@ module.exports = (knex) => {
         })
       ])
   })
+
+  router.post("/delete", (req, res) => {
+    console.log('req body: ', req.body);
+
+        Promise.all([
+        knex('points')
+        .where('point_id', req.body.point_id)
+        .del()
+        .then(function() {
+          res.status(201).send('full promises success');
+        })
+      ])
+  })
+
+  router.get("/:id", (req, res) =>{
+    Promise.all([
+    knex('points')
+      .where('point_id', req.params.id)
+      // .returning('point_name')
+      .then(function(onePoint) {
+        console.log('onePoint: ', onePoint)
+
+        res.json(onePoint[0])
+      })
+    ])
+})
+
 
   return router;
 
